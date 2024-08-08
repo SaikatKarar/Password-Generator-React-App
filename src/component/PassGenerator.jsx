94779670549859167e41475c243fa85b6110cefe
SaikatKarar <kararsaikat@gmail.com>
2024-08-08 21:55:28 +0530

    add functionality

--- src/component/PassGenerator.jsx
+++ src/component/PassGenerator.jsx
@@ -1,28 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactSwitch from 'react-switch'
 
 function PassGenerator() {
+    const [password, setPassword] = useState('');
+    const [passwordLength, setPasswordLength] = useState(8);
+    const [includeUpperCase, setIncludeUpperCase] = useState(true);
+    const [includeNumber, setIncludeNumber] = useState(true);
+    const [includeSpecialChars, setIncludeSpecialChars] = useState(true);
+
+    const generatePass = () => {
+        const lowerCaseChars = 'abcdefijklmnopqrstuvwxyz';
+        const upperCaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        const numbers = '0123456789';
+        const specialChars = '@#$&*';
+
+        let validChars = lowerCaseChars;
+
+        if (includeUpperCase) {
+            validChars += upperCaseChars;
+        }
+        if (includeNumber) {
+            validChars += numbers;
+        }
+        if (includeSpecialChars) {
+            validChars += specialChars;
+        }
+
+        let generatePassword = "";
+        for (let i = 0; i < passwordLength; i++) {
+            const randomIndex = Math.floor(Math.random() * validChars.length);
+            generatePassword += validChars.charAt(randomIndex);
+
+        }
+        setPassword(generatePassword);
+    }
+
+
     return (
         <div className='pt-8 text-center text-white font-bold'>
             <div className='flex flex-col items-center'>
                 <div className='bg-purple-800 w-[350px] shadow-2xl hover:shadow-blue-800 rounded-md p-3'>
                     <div className='mb-4 flex items-center justify-between'>
                         <label>Password Length: </label>
-                        <input type="number" className='w-12 bg-purple-700 pl-2' />
+                        <input
+                            value={passwordLength}
+                            onChange={(e) => setPasswordLength(e.target.value)}
+                            type="number" className='w-12 bg-purple-700 pl-2' />
                     </div>
                     <div className='mb-4 flex items-center justify-between'>
                         <label>Include Uppercase:</label>
-                        <ReactSwitch height={20} width={40} handleDiameter={18} />
+                        <ReactSwitch checked={includeUpperCase} onChange={() => setIncludeUpperCase(!includeUpperCase)} height={20} width={40} handleDiameter={18} />
                     </div>
                     <div className='mb-4 flex items-center justify-between'>
                         <label>Include Numbers:</label>
-                        <ReactSwitch height={20} width={40} handleDiameter={18} />
+                        <ReactSwitch checked={includeNumber} onChange={() => setIncludeNumber(!includeNumber)} height={20} width={40} handleDiameter={18} />
                     </div>
                     <div className='mb-4 flex items-center justify-between'>
                         <label>Include Special Characters:</label>
-                        <ReactSwitch height={20} width={40} handleDiameter={18} />
+                        <ReactSwitch checked={includeSpecialChars} onChange={() => setIncludeSpecialChars(!includeSpecialChars)} height={20} width={40} handleDiameter={18} />
                     </div>
-                    <button className='bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-600 hover:to-yellow-500 p-2 text-white font-bold rounded-md'>Generate Password</button>
+                    <button
+                        onClick={generatePass}
+                        className='bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-600 hover:to-yellow-500 p-2 text-white font-bold rounded-md'>Generate Password</button>
+                    {password && <p className='font-normal text-white mt-2'>Generate Password: <span className='font-bold'>{password}</span></p>}
                 </div>
             </div>
         </div>
